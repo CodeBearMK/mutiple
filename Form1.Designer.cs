42080165c36fa49cde5d2a13b4160f0b0122d574
cat32tw <mike1993xd@gmail.com>
2022-03-08 22:26:48 +0800

    第八章 CheckedListBox練習

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace mutiple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.clstTest = new System.Windows.Forms.CheckedListBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(23, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "請勾選 3 的倍數：";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMsg.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblMsg.Location = new System.Drawing.Point(229, 24);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(192, 74);
+            this.lblMsg.TabIndex = 2;
+            // 
+            // clstTest
+            // 
+            this.clstTest.FormattingEnabled = true;
+            this.clstTest.Location = new System.Drawing.Point(27, 85);
+            this.clstTest.Name = "clstTest";
+            this.clstTest.Size = new System.Drawing.Size(193, 144);
+            this.clstTest.TabIndex = 3;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(229, 113);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(84, 33);
+            this.btnNew.TabIndex = 4;
+            this.btnNew.Text = "出題";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(229, 152);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(84, 31);
+            this.btnCheck.TabIndex = 5;
+            this.btnCheck.Text = "核對";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(229, 189);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(84, 31);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "重答";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 244);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.clstTest);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.CheckedListBox clstTest;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mutiple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnCheck.Enabled = false;
+            btnClear.Enabled = false;
+            lblMsg.Text = "請按<出題>鈕開始";
+            clstTest.CheckOnClick = true;
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            clstTest.Items.Clear();
+            int num;
+            Random rnd = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                num = rnd.Next(1, 100);
+                while (clstTest.Items.Contains(num))
+                {
+                    num = rnd.Next(1, 5);
+                }
+                clstTest.Items.Add(num);
+
+            }
+            lblMsg.Text = "請開始做答";
+            btnNew.Enabled = false;
+            btnCheck.Enabled = true;
+            btnClear.Enabled = true;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            int right = 0;
+            for (int i = 0; i < clstTest.Items.Count; i++)
+            {
+                if (int.Parse(clstTest.Items[i].ToString()) % 3 == 0)
+                {
+                    if (clstTest.GetItemChecked(i))
+                    {
+                        right++;
+                    }
+                }
+                else
+                {
+                    if (!clstTest.GetItemChecked(i))
+                    {
+                        right++;
+                    }
+                }
+            }
+            lblMsg.Text = "答對 " + right.ToString() + " 題";
+            btnNew.Enabled = true;
+            btnCheck.Enabled = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            btnNew.Enabled = false;
+            btnCheck.Enabled = true;
+            for (int i = 0; i < clstTest.Items.Count; i++)
+            {
+                clstTest.SetItemChecked(i, false);
+            }
+            lblMsg.Text = "請重新作答";
+        }
+    }
+}
